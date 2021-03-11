491c42f002612b8705ea7db498f45460cf43f460
aniler0 <anil.er@hotmail.com>
2021-03-11 14:27:47 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GameScreen from "./components/GameScreen";
 import MainMenu from "./components/MainMenu";
 
